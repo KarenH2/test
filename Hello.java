6a4c34c014e1a8cff9d4d7c7273c169265a74cf4
jerry <jerry@hotmail.com>
2021-08-18 10:37:35 +0800

    新增計算方法

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello GitHub!");
+		System.out.println(calc(2,5));
+	}
+
+
+	public static int calc(int i,int j){
+		return i+j;
 	}
 }
